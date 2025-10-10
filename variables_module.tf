bd507eab10302c8c4dcd50f41b53ad249ff00af7
Espen Albert <EspenAlbert@users.noreply.github.com>
2025-10-10 10:13:23 +0100

    refactor: Updates auto_scaling defaults and validate no free tier cluster (#11)
    * refactor: change auto-scaling defaults to M10-M200
    
    * refactor: remove auto_scaling configuration from example modules (set by default)
    
    * feat: add validation for provider and instance size restrictions in cluster module
    
    * chore: remove examples from root readme will handle this in follow up PR
    
    * chore: re-generate docs
    
    * style: fmt
    
    * fix: connection_string_private_endpoint might not be set
    
    * chore: cluster name update
    
    * fix: update provider_name validation to allow only AWS/AZURE/GCP

--- variables_module.tf
+++ variables_module.tf
@@ -12,7 +12,6 @@ The simplest way to define your cluster topology:
 
 Note: The order in which region blocks are defined in this list determines their priority within each shard or zone. The first region gets priority 7 (maximum), the next 6, and so on (minimum 0).
 EOT
-
   type = list(object({
     name                    = optional(string)
     node_count              = optional(number)
@@ -24,6 +23,16 @@ EOT
     instance_size_analytics = optional(string)
     zone_name               = optional(string)
   }))
+
+  validation {
+    error_message = "Only provider_name AWS/AZURE/GCP are allowed."
+    condition     = length([for region in var.regions : region if region.provider_name != null && !contains(["AWS", "AZURE", "GCP"], region.provider_name)]) == 0
+  }
+
+  validation {
+    error_message = "M0, M2, and M5 are not allowed for this module. Use M10 or higher instead."
+    condition     = length([for region in var.regions : region if region.instance_size != null && (region.instance_size == "M0" || region.instance_size == "M2" || region.instance_size == "M5")]) == 0
+  }
 }
 
 variable "provider_name" {
@@ -31,6 +40,11 @@ variable "provider_name" {
   type        = string
   nullable    = true
   default     = null
+
+  validation {
+    error_message = "Only provider_name AWS/AZURE/GCP are allowed."
+    condition     = var.provider_name == null || contains(["AWS", "AZURE", "GCP"], var.provider_name)
+  }
 }
 
 variable "instance_size" {
@@ -38,6 +52,11 @@ variable "instance_size" {
   type        = string
   nullable    = true
   default     = null
+
+  validation {
+    error_message = "M0, M2, and M5 are not allowed for this module. Use M10 or higher instead."
+    condition     = var.instance_size == null || (var.instance_size != "M0" && var.instance_size != "M2" && var.instance_size != "M5")
+  }
 }
 
 variable "disk_size_gb" {
@@ -67,22 +86,26 @@ variable "instance_size_analytics" {
   type        = string
   nullable    = true
   default     = null
+  validation {
+    error_message = "M0, M2, and M5 are not allowed for this module. Use M10 or higher instead."
+    condition     = var.instance_size_analytics == null || (var.instance_size_analytics != "M0" && var.instance_size_analytics != "M2" && var.instance_size_analytics != "M5")
+  }
 }
 
 variable "auto_scaling" {
   description = "Auto scaling config for electable/read-only specs. Enabled by default with Architecture Center recommended defaults."
   type = object({
     compute_enabled            = optional(bool, true)
-    compute_max_instance_size  = optional(string, "M60")
-    compute_min_instance_size  = optional(string, "M30")
+    compute_max_instance_size  = optional(string, "M200")
+    compute_min_instance_size  = optional(string, "M10")
     compute_scale_down_enabled = optional(bool, true)
     disk_gb_enabled            = optional(bool, true)
   })
 
   default = {
     compute_enabled            = true
-    compute_max_instance_size  = "M60"
-    compute_min_instance_size  = "M30"
+    compute_max_instance_size  = "M200"
+    compute_min_instance_size  = "M10"
     compute_scale_down_enabled = true
     disk_gb_enabled            = true
   }
