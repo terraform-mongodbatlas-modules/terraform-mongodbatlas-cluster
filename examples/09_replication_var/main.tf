d9496a14fec4fe8937085eb9317b512e9b68a16b
Espen Albert <espen.albert@mongodb.com>
2025-10-20 17:18:08 +0100

    refactor: change examples to use SHARDED by default

--- examples/09_replication_var/main.tf
+++ examples/09_replication_var/main.tf
@@ -5,12 +5,13 @@ module "replication_var" {
   project_id             = var.project_id
   mongo_db_major_version = "8.0"
   regions                = []
-  cluster_type           = "REPLICASET"
+  cluster_type           = "SHARDED"
   replication_specs = [{
     region_configs = [{
       priority      = 7
       provider_name = "AWS"
       region_name   = "US_EAST_1"
+      shard_number  = 1
       electable_specs = {
         instance_size = "M10"
         node_count    = 3
