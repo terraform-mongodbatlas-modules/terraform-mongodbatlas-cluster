7d960f32c5dad06137cfd445687e35fdae80c1d6
Espen Albert <espen.albert@mongodb.com>
2026-07-21 10:31:59 +0200

    ensures regression files are formatted

--- tools/tf_gen/testdata/regressions/cloud_provider_access_setup_single/outputs.tf
+++ tools/tf_gen/testdata/regressions/cloud_provider_access_setup_single/outputs.tf
@@ -1,9 +1,9 @@
 output "cloud_provider_access_setup" {
   value = {
-    aws_config = mongodbatlas_cloud_provider_access_setup.this.aws_config
-    created_date = mongodbatlas_cloud_provider_access_setup.this.created_date
-    gcp_config = mongodbatlas_cloud_provider_access_setup.this.gcp_config
+    aws_config        = mongodbatlas_cloud_provider_access_setup.this.aws_config
+    created_date      = mongodbatlas_cloud_provider_access_setup.this.created_date
+    gcp_config        = mongodbatlas_cloud_provider_access_setup.this.gcp_config
     last_updated_date = mongodbatlas_cloud_provider_access_setup.this.last_updated_date
-    role_id = mongodbatlas_cloud_provider_access_setup.this.role_id
+    role_id           = mongodbatlas_cloud_provider_access_setup.this.role_id
   }
 }
